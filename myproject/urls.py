9f7487302111300f3cfe19676839f24239903188
Félix <f.leroux99@gmail.com>
2020-11-02 11:09:57 -0500

    V1.0

--- myproject/urls.py
+++ myproject/urls.py
@@ -1,12 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import path
 
-from pages.views import home_view
+from myproject.views import home_view
 
 urlpatterns = [
-    path('admin', admin.site.urls),
     path('', home_view, name='home'),
 ]
 
--- myproject/views.py
+++ myproject/views.py
@@ -1,6 +1,5 @@
-from django.contrib import messages
 from django.core.mail import send_mail
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 
 
 def suggestions_view(request, *args, **kwargs):
